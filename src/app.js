5c6f5c885cec653ab7153eb5f1839cffe6435bfe
srpillai <srpillai@hotmail.com>
2019-04-22 20:53:19 -0500

    bolierplate

--- src/app.js
+++ src/app.js
@@ -1,52 +1,6 @@
-class Header extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Budgetify</h1>
-          <h2>Put your money in the hands of a computer</h2>
-        </div>
-      );
-    }
-  }
-  
-  class Action extends React.Component {
-    render() {
-      return (
-        <div>
-          <button>How should I spend?</button>
-        </div>
-      );
-    }
-  }
-  
-  class Options extends React.Component {
-    render() {
-      return (
-        <div>
-          Options component here
-        </div>
-      );
-    }
-  }
-  
-  class AddOption extends React.Component {
-    render() {
-      return (
-        <div>
-          AddOption component here
-        </div>
-      );
-    }
-  }
-  
-  const jsx = (
-    <div>
-      <Header />
-      <Action />
-      <Options />
-      <AddOption />
-    </div>
-  );
-  
-  ReactDOM.render(jsx, document.getElementById('app'));
-  
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
